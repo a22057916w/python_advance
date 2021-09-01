972c41aab474c15ac1012f0acf616fad1d053b70
a22057916w <a22057916w@gmail.com>
2021-09-01 11:35:48 +0800

    hellow world example

--- Tkinter/hello.py
+++ Tkinter/hello.py
@@ -1,1 +1,19 @@
 import tkinter as tk
+
+# 定義一個視窗 名叫 window
+window = tk.Tk()
+
+# 設定標題
+window.title("whisky")
+
+# 設定像素大小
+window.geometry("600x800")
+
+# 宣告一個標籤
+label_1 = tk.Label(window, text="Hello world", bg="yellow", fg="#263238", font=("Arial", 12))
+
+# 設定放置的位置 ( 使用 grid 佈局 )
+label_1.grid(column=0, row=0)
+
+# 主視窗迴圈顯示
+window.mainloop()
