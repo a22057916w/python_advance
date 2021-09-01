f4e389d168d4bf59b181b0891ddb512e2686490b
a22057916w <a22057916w@gmail.com>
2021-09-01 16:57:43 +0800

    deving

---
+++ Tkinter/menu.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("MenuBar")
+window.geometry("1600x900")
+
+menuBar = tk.Menu(window)
+window.config(menu = menuBar)
+
+menuFile = tk.Menu(menuBar)
+menuHelp = tk.Menu(menuBar)
+
+menuBar.add_cascade(label="File", menu=menuFile)
+menuBar.add_cascade(label="Help", menu=menuHelp)
+
+menuFile.add_command(label="New")
+menuFile.add_command(label="Open...")
+menuFile.add_separator()
+menuFile.add_command(label="Exit", command=window.quit)
+
+menuHelp.add_command(label="about")
+
+window.mainloop()
