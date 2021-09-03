22d4fbc145986e51da7010b9048c965d08b7b105
a22057916w <a22057916w@gmail.com>
2021-09-03 14:50:25 +0800

    dev read _ini and GUI

--- simple_calculator/cal.py
+++ simple_calculator/cal.py
@@ -2,22 +2,29 @@ from configparser import ConfigParser
 import tkinter as tk
 import os
 
-# global var for .ini file info
+# --------- global var for .ini file info ----------
+# read operands
 nInitValue = 0
 nFirstOperand = 0
 nSecondOperand = 0
 nThirdOperand = 0
 nFourthOperand = 0
-pass
 
-# global var for cal_UI
-expression = ""
+# read strOperator
+strFirstOp = ""
+strSecondOp = ""
+strThirdOp = ""
+strFourthOp = ""
+
+
 
 # read section "Setting" in .ini file
 def read_ini_section(section):
     try:
         global nInitValue, nFirstOperand, nSecondOperand, nThirdOperand, nFourthOperand
+        global strFirstOp, strSecondOp, strThirdOp, strFourthOp
 
+        # ---------- read operands ----------
         if "initValue" in section:
             nInitValue = section.getint("initValue")
             print("get initValue %d success" % nInitValue)
@@ -43,6 +50,29 @@ def read_ini_section(section):
             print("get fourthOperand %d success" % nFourthOperand)
         else:
             print("get fourthOperand fail")
+
+        # ---------- read oprators -----------
+        if "firstOperator" in section:
+            strFirstOp = section.get("firstOperator")
+            print("successfully get firstOperator %s" % strFirstOp)
+        else:
+            print("get firstOperator fail")
+        if "secondOperator" in section:
+            strSecondOp = section.get("secondOperator")
+            print("successfully get secondOperator %s" % strSecondOp)
+        else:
+            print("get secondOperator fail")
+        if "thirdOperator" in section:
+            strThirdOp = section.get("thirdOperator")
+            print("successfully get thirdOperator %s" % strThirdOp)
+        else:
+            print("get thirdOperator fail")
+        if "fourthOperator" in section:
+            strFourthOp = section.get("fourthOperator")
+            print("successfully get fourthOperator %s" % strFourthOp)
+        else:
+            print("get fourthOperator fail")
+
     except Exception as e:
         print(e)
 
@@ -62,32 +92,41 @@ def operation(strOperator, nOperand):
     strResult.set(str(nTmp))
 
 # create calculator GUI
-def cal_UI(nInitValue, nFirstOperand, nSecondOperand, nThirdOperand, nFourthOperand):
+def cal_UI(nInitValue, nFirstOperand, nSecondOperand, nThirdOperand, nFourthOperand, strFirstOp, strSecondOp, strThirdOp, strFourthOp):
     global strResult
 
     window = tk.Tk()
     window.title("Simple Calculator")
-    window.geometry("600x800")
 
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=1)
+
+
+    # need to be modified
+    for i in range(3):
+        window.rowconfigure(i, weight=1)
+
+    # 將 StringVar 變數與 Tkinter 控制元件關聯後，修改 StringVar 變數後，Tkinter 將自動更新此控制元件
     strResult = tk.StringVar()
     strResult.set(str(nInitValue))
-    print(type(strResult))
+    #print(type(strResult))
 
+    # 使用Label顯示計算值
     lbResult = tk.Label(window, textvariable=strResult)
-    lbResult.pack()
+    lbResult.grid(row = 0, column = 0, columnspan=2, ipadx=70, sticky=tk.W+tk.E)
 
     # -------- button setup ---------
-    btnFirst = tk.Button(window, width=20, text=str(nFirstOperand), command=operation("+", nFirstOperand))
-    btnFirst.pack(side="left")
+    btnFirst = tk.Button(window, width=20, text=strFirstOp + str(nFirstOperand), command=lambda: operation(strFirstOp, nFirstOperand))
+    btnFirst.grid(row=1, column=0, sticky=tk.NW+tk.SE)
 
-    btnSecond = tk.Button(window, width=20, text=str(nSecondOperand), command=operation("-", nSecondOperand))
-    btnSecond.pack(side="right")
+    btnSecond = tk.Button(window, width=20, text=strSecondOp + str(nSecondOperand), command=lambda: operation(strSecondOp, nSecondOperand))
+    btnSecond.grid(row=1, column=1, sticky=tk.NW+tk.SE)
 
-    btnThird = tk.Button(window, width=20, text=str(nThirdOperand), command=operation("+", nThirdOperand))
-    btnThird.pack(side="left")
+    btnThird = tk.Button(window, width=20, text=strThirdOp + str(nThirdOperand), command=lambda: operation(strThirdOp, nThirdOperand))
+    btnThird.grid(row=2, column=0, sticky=tk.NW+tk.SE)
 
-    btnFourth = tk.Button(window, width=20, text=str(nFourthOperand), command=operation("*", nFourthOperand))
-    btnFourth.pack(side="right")
+    btnFourth = tk.Button(window, width=20, text=strFourthOp + str(nFourthOperand), command=lambda: operation(strFourthOp, nFourthOperand))
+    btnFourth.grid(row=2, column=1, sticky=tk.NW+tk.SE)
 
     window.mainloop()
 
@@ -104,8 +143,7 @@ if __name__ == '__main__':
         read_ini_section(config["Setting"])
 
     print(nInitValue, nFirstOperand, nSecondOperand, nThirdOperand, nFourthOperand)
+    print(strFirstOp, strSecondOp, strThirdOp, strFourthOp)
 
-    # 將 StringVar 變數與 Tkinter 控制元件關聯後，修改 StringVar 變數後，Tkinter 將自動更新此控制元件
 
-
-    cal_UI(nInitValue, nFirstOperand, nSecondOperand, nThirdOperand, nFourthOperand)
+    cal_UI(nInitValue, nFirstOperand, nSecondOperand, nThirdOperand, nFourthOperand, strFirstOp, strSecondOp, strThirdOp, strFourthOp)
