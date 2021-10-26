3344126c3e456ee4ff17dd7005e8d1da6d82b1cb
a22057916w <a22057916w@gmail.com>
2021-10-26 14:51:02 +0800

    add printLog, mod arch

--- LogParser/LTE.py
+++ LogParser/LTE.py
@@ -22,6 +22,7 @@
 ##==============================================================================
 import re
 import os
+import sys
 import pandas as pd
 import codecs
 import time
@@ -32,20 +33,41 @@ g_strVersion = "3.0.0.1"
 #[ParseLogPath]
 g_strLogDir = "./TryingLog"
 
-def getDateTimeFormat():
-    strDateTime = "[%s]" % (time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
-    return strDateTime
 
-def printLog(strPrintLine):
-    strFileName = os.path.basename(__file__).split('.')[0]
-    fileLog = codecs.open(strFileName, 'a', "utf-8")
-    print(strPrintLine)
-    fileLog.write("%s%s\r\n" % (getDateTimeFormat(), strPrintLine))
-    fileLog.close()
+# ----------- read and parse target log ----------
+def parseLog(strSNDLog):
+    printLog("[I][parseLog] ------- Start Parsing Log -------")
 
+    listLTE, listZigbee = [], []
+    try:
+        for strSNDir in listSNLogs:
+            strSNLog = os.path.join(g_strLogDir, strSNDir)
 
-# ----------- read and parse target log--------
+            # iterate through log files in a SN folder (second layer)
+            for strLog in os.listdir(strSNLog):
+                strLog = os.path.join(strSNLog, strLog)
+
+                # parse GFI20_RF_LTE.log files
+                reMatch = re.fullmatch("^.*RF_LTE\.log", strLog)
+                if(reMatch != None):
+                    dictLTE = parseLTE(strLog)
+                    dictLTE.update({"SN" : strSNDir})   # update() for concat two dicts
+                    listLTE.append(dictLTE)
+
+                # parse GFI20_RF_Zigbee.log files
+                reMatch = re.fullmatch("^.*RF_Zigbee\.log", strLog)
+                if(reMatch != None):
+                    dictZigbee = parseZigbee(strLog)
+                    dictZigbee.update({"SN" : strSNDir})
+                    listZigbee.append(dictZigbee)
+        printLog("[I][parseLog] ------- Finish Parsing Log -------")
+    except Exception as e:
+        printLog("[E][parseLog] Unexpected Error: " + str(e))
+    return listLTE, listZigbee
+
 def parseLTE(strLTEPath):
+    printLog("[I][parseLTE] Parse LTE log: %s" % strLTEPath)
+
     dictLTE = {
         "dBm_CH9750" : None,
         "dBm_CH2787" : None,
@@ -54,48 +76,51 @@ def parseLTE(strLTEPath):
         "Current_mA_3G_CH2787" : None,
         "Current_mA_2G_CH124" : None,
         "dBm_CH124" : None }
+    try:
+        nPowerCase, nCurrentCase = 1, 1
+        with open(strLTEPath, encoding='big5') as log:  # big5 for windows
+            content = log.readlines()
+            for line in content:
+                #strPower = "-1e9"
+                #strCurrent = "-1e9"
 
-    nPowerCase = 1
-    nCurrentCase = 1
-    with open(strLTEPath, encoding='big5') as log:  # big5 for windows
-        content = log.readlines()
-        for line in content:
-            #strPower = "-1e9"
-            #strCurrent = "-1e9"
+                if "Power: " in line:
+                    # get the figure of the line "Power: 31.718\n"
+                    strPower = line.split(": ")[1].strip(" \n")
+                    #print(strPower)
+                    if nPowerCase == 1:
+                        dictLTE["dBm_CH2787"] = strPower
+                    elif nPowerCase == 2:
+                        dictLTE["dBm_CH9750"] = strPower
+                    elif nPowerCase == 3:
+                        dictLTE["dBm_2G_CH124"] = strPower
+                    nPowerCase += 1
 
-            if "Power: " in line:
-                # get the figure of the line "Power: 31.718\n"
-                strPower = line.split(": ")[1].strip(" \n")
-                #print(strPower)
-                if nPowerCase == 1:
-                    dictLTE["dBm_CH2787"] = strPower
-                elif nPowerCase == 2:
-                    dictLTE["dBm_CH9750"] = strPower
-                elif nPowerCase == 3:
-                    dictLTE["dBm_2G_CH124"] = strPower
-                nPowerCase += 1
+                if "Current: " in line:
+                    # get the figure of the line "Current: 0.246 A\n"
+                    strCurrent = line.split(": ")[1].strip(" A\n")
+                    #print(strCurrent)
+                    if nCurrentCase == 1:
+                        dictLTE["Current_mA_3G_CH2787"] = str(eval(strCurrent) * 1000)
+                    elif nCurrentCase == 2:
+                        dictLTE["Current_mA_3G_CH9750"] = str(eval(strCurrent) * 1000)
+                    elif nCurrentCase == 3:
+                        dictLTE["Current_mA_2G_CH124"] = str(eval(strCurrent) * 1000)
+                    nCurrentCase += 1
 
-            if "Current: " in line:
-                # get the figure of the line "Current: 0.246 A\n"
-                strCurrent = line.split(": ")[1].strip(" A\n")
-                #print(strCurrent)
-                if nCurrentCase == 1:
-                    dictLTE["Current_mA_3G_CH2787"] = str(eval(strCurrent) * 1000)
-                elif nCurrentCase == 2:
-                    dictLTE["Current_mA_3G_CH9750"] = str(eval(strCurrent) * 1000)
-                elif nCurrentCase == 3:
-                    dictLTE["Current_mA_2G_CH124"] = str(eval(strCurrent) * 1000)
-                nCurrentCase += 1
-
-            if "Rx RSSI: " in line:
-                # get the figure of the line "Rx RSSI: -15 dBm\n"
-                strRSSI = line.split(": ")[1].strip(" dbm\n")
-                dictLTE["dBm_CH124"] = strRSSI
-                break
+                if "Rx RSSI: " in line:
+                    # get the figure of the line "Rx RSSI: -15 dBm\n"
+                    strRSSI = line.split(": ")[1].strip(" dbm\n")
+                    dictLTE["dBm_CH124"] = strRSSI
+                    break
+    except Exception as e:
+        printLog("[E][parseLTE] Unexpected Error: " + str(e))
     return(dictLTE)
 
 def parseZigbee(strZigBeePath):
-    dictZigbee ={
+    printLog("[I][parseZigbee] Parse Zigbee log: %s" % strZigBeePath)
+
+    dictZigbee = {
         "Power_dBm_CH15" : None,
         "Power_dBm_CH21" : None,
         "Power_dBm_CH24" : None,
@@ -106,7 +131,6 @@ def parseZigbee(strZigBeePath):
         "Current_mA_CH24" : None }
     try:
         nPowerCase , nCurrentCase, nLNACase = 1, 1, 1
-
         with open(strZigBeePath, encoding="big5") as Zigbee:    # big5 for windows
             content = Zigbee.readlines()
             for line in content:
@@ -148,8 +172,7 @@ def parseZigbee(strZigBeePath):
                     nLNACase += 1
 
     except Exception as e:
-        print(str(e))
-
+        printLog("[E][parseZigbee] Unexpected Error: " + str(e))
     return(dictZigbee)
 
 def save(listLTE, listZigbee):
@@ -165,36 +188,39 @@ def save(listLTE, listZigbee):
     dfLogInfo.to_excel(writer)
     writer.save()
 
-    print(dfLogInfo)
+    #print(dfLogInfo)
+
+
+def getDateTimeFormat():
+    strDateTime = "[%s]" % (time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
+    return strDateTime
+
+def printLog(strPrintLine):
+    strFileName = os.path.basename(__file__).split('.')[0]
+    fileLog = codecs.open(g_strFileName + ".log", 'a', "utf-8")
+    print(strPrintLine)
+    fileLog.write("%s%s\r\n" % (getDateTimeFormat(), strPrintLine))
+    fileLog.close()
+
 
 if __name__ == "__main__":
+    global g_strFileName
+    g_strFileName = os.path.basename(__file__).split('.')[0]
+
+    printLog("========== Start ==========")
+    printLog("[I][main] Python " + sys.version)
+    printLog("[I][main] %s.py %s" % (g_strFileName, g_strVersion))
+
     listLTE, listZigbee = [], []
-    dictInfo = {"SN" : None}
     # ------------ find the target file --------------
     try:
-        # get file names of TryingLog (first layer)
+        # get directory names of TryingLog (first layer)
         listSNLogs = os.listdir(g_strLogDir)
+        # iterate through log files in a SN folder (second layer)
+        listLTE, listZigbee = parseLog(listSNLogs)
 
-        for strSNDir in listSNLogs:
-            strSNLog = os.path.join(g_strLogDir, strSNDir)
 
-            # iterate through log files in a SN folder (second layer)
-            for strLog in os.listdir(strSNLog):
-                strLog = os.path.join(strSNLog, strLog)
-                # parse GFI20_RF_LTE.log files
-                reMatch = re.fullmatch("^.*RF_LTE\.log", strLog)
-                if(reMatch != None):
-                    dictLTE = parseLTE(strLog)
-                    dictLTE.update({"SN" : strSNDir})   # update() for concat two dicts
-                    listLTE.append(dictLTE)
-                # parse GFI20_RF_Zigbee.log files
-                reMatch = re.fullmatch("^.*RF_Zigbee\.log", strLog)
-                if(reMatch != None):
-                    dictZigbee = parseZigbee(strLog)
-                    dictZigbee.update({"SN" : strSNDir})
-                    listZigbee.append(dictZigbee)
-
-            #print(listLTE)
+        print(listLTE)
 
         save(listLTE, listZigbee)
     except Exception as e:
