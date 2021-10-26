85d2b09630fe0701b3e0b6e08b4fd07e6f5c6bbe
a22057916w <a22057916w@gmail.com>
2021-10-26 10:09:43 +0800

    deving parseZIgbee()

--- LogParser/LTE.py
+++ LogParser/LTE.py
@@ -59,14 +59,15 @@ def readLog():
 
                 # parse GFI20_RF_LTE.log files
                 listLTE = []
-                reMatch = re.fullmatch("^.*RF_LTE\.log", strLog, re.I)
+                reMatch = re.fullmatch("^.*RF_LTE\.log", strLog)
+                #if(reMatch != None):
+                    #print(strLog)
+                    #listLTE.append(parseLTE(strLog))
+                reMatch = re.fullmatch("^.*RF_Zigbee\.log", strLog)
                 if(reMatch != None):
                     print(strLog)
-                    parseLTE(strLog)
+                    parseZigbee(strLog)
                     break
-                else:
-                    pass
-                #print("____________")
 
             break
     except Exception as e:
@@ -74,7 +75,6 @@ def readLog():
 
 # ----------- read and parse target log--------
 def parseLTE(strLTEPath):
-    #print("SDfsdfsdf")
     dictLTE = {
         "dBm_CH9750" : None,
         "dBm_CH2787" : None,
@@ -84,19 +84,18 @@ def parseLTE(strLTEPath):
         "Current_mA_2G_CH124" : None,
         "dBm_CH124" : None }
 
-    listLTElogs = []
     nPowerCase = 1
     nCurrentCase = 1
     with open(strLTEPath, encoding='big5') as log:  # big5 for windows
         content = log.readlines()
         for line in content:
-            strPower = "-1e9"
-            strCurrent = "-1e9"
+            #strPower = "-1e9"
+            #strCurrent = "-1e9"
 
             if "Power: " in line:
                 # get the figure of the line "Power: 31.718\n"
                 strPower = line.split(": ")[1].strip(" \n")
-                print(strPower)
+                #print(strPower)
                 if nPowerCase == 1:
                     dictLTE["dBm_CH2787"] = strPower
                 elif nPowerCase == 2:
@@ -106,17 +105,70 @@ def parseLTE(strLTEPath):
                 nPowerCase += 1
 
             if "Current: " in line:
+                # get the figure of the line "Current: 0.246 A\n"
                 strCurrent = line.split(": ")[1].strip(" A\n")
-                print(strCurrent)
+                #print(strCurrent)
                 if nCurrentCase == 1:
                     dictLTE["Current_mA_3G_CH2787"] = str(eval(strCurrent) * 1000)
                 elif nCurrentCase == 2:
                     dictLTE["Current_mA_3G_CH9750"] = str(eval(strCurrent) * 1000)
                 elif nCurrentCase == 3:
-                    dictLTE["Current_mA_2G_CH124"] =str(eval(strCurrent) * 1000)
+                    dictLTE["Current_mA_2G_CH124"] = str(eval(strCurrent) * 1000)
                 nCurrentCase += 1
 
-    print(dictLTE)
+            if "Rx RSSI: " in line:
+                strRSSI = line.split(": ")[1].strip(" dbm\n")
+                dictLTE["dBm_CH124"] = strRSSI
+                break
+    return(dictLTE)
+
+def parseZigbee(strZigBeePath):
+    dictZigbee ={
+        "Power_dBm_CH15" : None,
+        "Power_dBm_CH21" : None,
+        "Power_dBm_CH24" : None,
+        "dBm_LNA_ON" : None,
+        "dBm_LNA_Off" : None,
+        "Current_mA_CH15" : None,
+        "Current_mA_CH21" : None,
+        "Current_mA_CH24" : None }
+    try:
+        nPowerCase = 1
+        nCurrentCase = 1
+        with open(strZigBeePath, encoding="big5") as Zigbee:    # big5 for windows
+            content = Zigbee.readlines()
+            for line in content:
+                #strPower = "-1e9"
+                #strCurrent = "-1e9"
+
+                if "Power: " in line:
+                    # get the figure of the line "Power: 8.817 dBm\n"
+                    strPower = line.split(": ")[1].strip(" dBm\n")
+                    #print(strPower)
+                    if nPowerCase == 1:
+                        dictLTE["Power_dBm_CH15"] = strPower
+                    elif nPowerCase == 2:
+                        dictLTE["Power_dBm_CH21"] = strPower
+                    elif nPowerCase == 3:
+                        dictLTE["Power_dBm_CH24"] = strPower
+                    nPowerCase += 1
+
+                if "Current: " in line:
+                    # get the figure of the line "Current: 0.081 A\n"
+                    strCurrent = line.split(": ")[1].strip(" A\n")
+                    #print(strCurrent)
+                    if nCurrentCase == 1:
+                        dictLTE["Current_mA_CH15"] = str(eval(strCurrent) * 1000)
+                    elif nCurrentCase == 2:
+                        dictLTE["Current_mA_CH21"] = str(eval(strCurrent) * 1000)
+                    elif nCurrentCase == 3:
+                        dictLTE["Current_mA_CH24"] = str(eval(strCurrent) * 1000)
+                    nCurrentCase += 1
+
+    except Exception as e:
+        print(str(e))
+
+    print(dictZigbee)
 
 if __name__ == "__main__":
     readLog()
