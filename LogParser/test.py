a18a58197653d7d6dd1de61af557e9c194b59212
a22057916w <a22057916w@gmail.com>
2021-11-03 15:05:17 +0800

    thinking pesudo

--- LogParser/test.py
+++ LogParser/test.py
@@ -4,7 +4,7 @@ with open("/data/Code/python/python_advance/LogParser/singleLog/6011643700031_12
     content = Zigbee.readlines()
     search(strblock)
 
-def serach(dict, strblock, content, key, postfix)
+def serach(content, regex, key, postfix)
     for line in content:
         if exist strblock info:
             search(key,value)
