a1474fb674f580ef5538d56a0233b9bf82a3eed3
a22057916w <a22057916w@gmail.com>
2021-11-22 10:27:15 +0800

    acclaim the copyright

---
+++ Auto_RRSIandWiFiSAR_Report/report.py
@@ -0,0 +1,21 @@
+##! python3
+##==============================================================================
+##    Copyright (c) 2021 COMPAL Electronic Inc. All rights reserved.
+##    This program contains proprietary and confidential information.
+##    All rights reserved except as may be permitted by prior written consent.
+##
+##    Compal STiD NPSD Test Program Release Notification.
+##
+##    ModuleName:
+##            report.py
+##
+##    Abstract:
+##
+##
+##    Author:
+##            22-Nov-2021 Willy Chen
+##
+##    Revision History:
+##            Rev 1.0.0.1 22-Nov-2021 Willy
+##                    First create.
+##==============================================================================
