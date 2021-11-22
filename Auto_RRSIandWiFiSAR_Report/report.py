4c67be8d486ed9ee11573c5e3bf677066dea9fab
a22057916w <a22057916w@gmail.com>
2021-11-22 10:29:26 +0800

    setup default functions

--- Auto_RRSIandWiFiSAR_Report/report.py
+++ Auto_RRSIandWiFiSAR_Report/report.py
@@ -30,3 +30,63 @@ from openpyxl.styles import PatternFill, Alignment, Font
 from openpyxl.styles.borders import Border, Side
 import re
 import time, logging
+
+
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+
+# Update UI Process (%)
+def updateWebpageInfo(nProgress=g_nProgressC, strWebpageInfo=None):
+    global g_nProgressC
+    strWebComPath = os.path.join(logPath, "WebComunicate_%s.txt" % strUser)
+    if nProgress != g_nProgressC:
+        g_nProgressC = nProgress
+
+    if nProgress == 0:
+        ## Renew WebComunicate.txt
+        with open(strWebComPath, 'w') as f:
+            f.write('')
+    else:
+        strToWrite = "%s;%s" % (nProgress, strWebpageInfo)
+        with open(strWebComPath, 'w') as f:
+            f.write(strToWrite)
+
+
+def getDateTimeFormat():
+    strDateTime = "%s" % (time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()))
+    return strDateTime
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Function setup as many loggers as you want"""
+
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(asctime)s][%(levelname)-5s][%(lineno)-4d][%(funcName)s] %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+    fh.setFormatter(formatter)
+
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    ch = logging.StreamHandler(sys.stdout)
+    # ch = logging.StreamHandler()
+    ch.setLevel(logging.CRITICAL)
+
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(levelname)-5s - %(lineno)-4d - %(funcName)s : %(message)s')
+    # tell the handler to use this format
+    ch.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
