c817f2fe1aef6be9123e6e2839b4b48fd9a9f733
naitea9527 <a22057916w@gmail.com>
2021-11-26 14:44:29 +0800

    test json.load()

--- Auto_RRSIandWiFiSAR_Report/test.py
+++ Auto_RRSIandWiFiSAR_Report/test.py
@@ -1,16 +1,9 @@
 import os, time
 import datetime as df
+import json
 
-dir = "./test_SN"
-for file in os.listdir(dir):
-    print(file)
-    c_time = os.path.getctime(os.path.join(dir, file))
-    local_time = df.datetime.fromtimestamp(c_time)
-    print(local_time.strftime("%Y-%m-%d %H:%M:%S"))
-
-test_time = df.datetime.strptime("2021-11-27 13:13:13", "%Y-%m-%d %H:%M:%S")
-print(type(test_time), type(local_time))
-if test_time > local_time:
-    print(test_time)
-else:
-    print(local_time.strftime("%Y-%m-%d %H:%M:%S"))
+mapJson = ".\mapping\sanchezPeng\mapping.json"
+with open(mapJson, "r") as mappingJson:
+    dict = dict(json.load(mappingJson))
+    print(dict["starttime"])
+    print(type(json))
