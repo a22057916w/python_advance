9e4df75e3ef3f3824444ac69ff8f9542d9556149
a22057916w <a22057916w@gmail.com>
2022-01-26 10:37:41 +0800

    test pptx package

---
+++ pptx/test.py
@@ -0,0 +1,12 @@
+from pptx import Presentation
+
+prs = Presentation()
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+
+title.text = "Fuck Off"
+subtitle.text = "Will You?"
+
+prs.save("test.pptx")
