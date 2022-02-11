0e7d169a20e7482742187b8bb823fdee884e44ba
a22057916w <a22057916w@gmail.com>
2022-02-11 15:39:04 +0800

    test prs.slides object

--- pptx/presentation.py
+++ pptx/presentation.py
@@ -14,4 +14,12 @@ subtitle = slide.placeholders[1]
 title.text = "Fuck Off"
 subtitle.text = "Will You?"
 
+prs.slides.add_slide(title_slide_layout)
+print(len(prs.slides))
+
+# for shape in slide.shapes:
+#     if shape.is_placeholder:
+#         phf = shape.placeholder_format
+#         print('%d, %s' % (phf.idx, phf.type))
+
 prs.save("./result/Presentation.pptx")
