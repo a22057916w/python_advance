f67129b6f4c1a3821f273decf18b0f40afce12c3
a22057916w <a22057916w@gmail.com>
2022-02-25 17:04:16 +0800

    test xml approach

--- pptx/test.py
+++ pptx/test.py
@@ -11,7 +11,9 @@ from pptx.util import Inches, Pt, Cm
 from pptx.dml.color import RGBColor
 from pptx.enum.text import PP_ALIGN
 #from pptx.oxml.shapes.table import CT_Table
+
 import xml.etree.ElementTree as ET
+from lxml import etree
 
 sys.path.append("./script")
 from PPTX_FEATURE import Report, Font, TableDataFrame
@@ -36,22 +38,50 @@ def get_table_xml(strPPTXFilePath):
     with open("table_xml.xml", "w") as f:
         f.write(str(table._tbl.xml))
 
-    cp_tbl = clientRT.add_table(table, 0)
+    cp_table = clientRT.add_table(table, 0)
     with open("copy_table_xml.xml", "w") as f:
-        f.write(str(cp_tbl._tbl.xml))
+        f.write(str(cp_table._tbl.xml))
 
     root = ET.fromstring(table._tbl.xml)
-    #print(root.tag)
-    test_xml(root)
+    cp_table_root = ET.fromstring(cp_table._tbl.xml)
+    #print(ET.iselement(root))
+    #print(list(root.iter())[0])
+    #test_xml(root, cp_table_root)
+    #print_xml_tag(root)
+    namespaces = {'a': 'http://schemas.openxmlformats.org/drawingml/2006/main'}
+    for row in range(len(table.rows)):
+        cell = table.cell(row, 0)
+        tc = cell._tc
+        txBody = tc.txBody
+        pPr = txBody.find(".//a:pPr", namespaces)
+        print(pPr.tag)
+        #tcPr = tc.get_or_add_tcPr()
+        # for child in tc:
+        #     print(child.tag)
+        # e_txBody = tc.find("a:txBody", namespaces)
+        # print(e_txBody.tag)
 
     clientRT.save("./result/test.pptx")
 
-def test_xml(root):
-    print("=================================")
+
+def test_xml(root, cp_table_root):
+    #print("=================================")
+
     for cld in root:
         print(cld.tag)
-    pass
+    print("=================================")
+    for cld in cp_table_root:
+        print(cld.tag)
 
+def print_xml_tag(root):
+    namespaces = {'a': 'http://schemas.openxmlformats.org/drawingml/2006/main'}
+    for tr in root.findall("a:tr", namespaces):
+        for tc in tr.findall("a:tc", namespaces):
+            for txbody in tc.findall("a:txbody", namespaces):
+                for p in txbody.findall("a:p", namespaces):
+                    for pPr in p.findall("a:pPr", namespaces):
+                        for defRPr in pPr.findall("a:defRPr", namespaces):
+                            print(defRPr.attrib)
 
 if __name__ == "__main__":
     print("sdfsdf")
