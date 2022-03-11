89ebff1f3941b0764c7be82ba6ead5d38bbefedb
a22057916w <a22057916w@gmail.com>
2022-03-11 14:49:26 +0800

    dev dataframe extra feature for client excel

--- pptx/script/EXCEL_FEATURE.py
+++ pptx/script/EXCEL_FEATURE.py
@@ -1,36 +1,52 @@
 import pandas as pd
 import numpy as np
 
-class Workbook():
+class DataFrameFeature():
 
-    def __init__(self, path = None):
-        country = pd.read_excel(path, sheet_name="Post-RTS", skiprows=1)
+    # truncate df according to the value in given column
+    @staticmethod
+    def truncate(df, *, column, first_value, last_value):
+        first_row = last_row = -1
+        n_col = column
 
-        df_postRTS = self.truncate_df(country, "P-RTS Country (Voluntary):", 0)
-        #print(df_postRTS)
-        df_postRTS = self.drop_na_row(df_postRTS)
-        print(df_postRTS)
-
-    def truncate_df(self, df, str_value, n_col, *, first_row = 0):
-        last_row = -1
+        b_first = False
 
         row = df.shape[0]   # return df row number
         for i in range(row):
-            if df.iloc[i, n_col] == str_value:
-                last_row = i - 1    # don't need the row we found
+
+            if df.iloc[i, n_col] == first_value:
+                b_first = True
+                first_row = i + 1
+
+            if last_value == None and pd.isna(df.iloc[i, n_col]):
+                #print(i)
+                last_row = i - 1
+                #print(df.iloc[i, :])
+            elif df.iloc[i, n_col] == last_value:
+                last_row = i - 1    # we don't need the row we found
+
+            if b_first and first_row < last_row:
+                #print(first_row, last_row)
+                #print("sdfsdfs")
                 break
 
-        return df.truncate(before=first_row, after=last_row)
+        return df.truncate(before=first_row, after=last_row).reset_index(drop=True)
 
+    # drop row that has NaN in each column
+    @staticmethod
     def drop_na_row(self, df):
-        b_allna = False
-
         drop_list = []
 
         row = df.shape[0]
         for i in range(row):
-            s = pd.Series(df.iloc[i, :])
+            s = pd.Series(df.iloc[i, :])    # trun df column into Series
+
+            # if all column value is NaN, drop the row
             if s.isnull().sum() == len(df.columns):
                 drop_list.append(i)
 
-        return df.drop(drop_list)
+        return df.drop(drop_list).reset_index(drop=True)
+
+    @staticmethod
+    def drop_row(self, df, idx):
+        return df.drop(idx).reset_index(drop=True)
